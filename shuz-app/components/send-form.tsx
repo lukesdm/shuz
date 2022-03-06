a0fbc563c4fbdd9ebfafceccb1902530578eaeed
lukesdm <2824165+lukesdm@users.noreply.github.com>
2022-03-06 21:13:41 +0100

    Fix tweaks

--- shuz-app/components/send-form.tsx
+++ shuz-app/components/send-form.tsx
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { OnResultFunction, QrReader } from 'react-qr-reader';
 
-function sendMessage(sender: string, receiverId: string, message: string) {
-  console.log(`Sending message: '${message}' from ${sender} to ${receiverId}`);
+function sendMessage(sender: string, receiverId: string, content: string) {
+  console.log(`Sending message: '${content}' from ${sender} to ${receiverId}`);
   
   // Fire and forget for now. Should put some error handling etc. around this.
-  fetch('/api/message', { method: 'POST', body: JSON.stringify({ sender, receiverId, message })});
+  fetch('/api/message', { method: 'POST', body: JSON.stringify({ sender, receiverId, content })});
 }
 
 export function SendForm() {
-  const [message, setMessage] = useState('');
+  const [content, setContent] = useState('');
   const [receiverId, setReceiverId] = useState('');
   const [showQrReader, setShowQrReader] = useState(false);
   const [messageSent, setMessageSent] = useState(false);
 
   useEffect(() => {
-    const canSendMessage = showQrReader && receiverId !== '' && message !== '';
+    const canSendMessage = showQrReader && receiverId !== '' && content !== '';
     if (canSendMessage) {
-      sendMessage("Testface", receiverId, message);
+      sendMessage("Testface", receiverId, content);
       
       setShowQrReader(false);
       setReceiverId('');
-      setMessage('');
+      setContent('');
       setMessageSent(true);
     }
-  }, [showQrReader, receiverId, message]);
+  }, [showQrReader, receiverId, content]);
 
   const onQrRead: OnResultFunction = (result, error) => {
     if (!!result) {
@@ -40,12 +40,12 @@ export function SendForm() {
   }
 
   return (
-    <form>
+    <form onSubmit={e => e.preventDefault()}>
       <label>
         Message:
-        <input type="text" name="message" onChange={e => setMessage(e.target.value)} />
+        <input type="text" name="content" onChange={e => setContent(e.target.value)} />
       </label>
-      <input type="button" value="Send (start QR capture)" name="start-send" onClick={onSendClick} />
+      <input type="button" value="Send" name="start-send" onClick={onSendClick} />
       {showQrReader && <h3>Scan recipient&apos;s QR code</h3>}
       {showQrReader && <QrReader onResult= {onQrRead} constraints = {{}} />}
       {messageSent && <h3>Message sent!</h3>}
