f03229868e7e0aa61720b591a748dd707c2df8fe
lukesdm <2824165+lukesdm@users.noreply.github.com>
2022-03-11 21:04:46 +0100

    New message fade in. WIP - it's a bit slow

--- shuz-app/components/receiver.tsx
+++ shuz-app/components/receiver.tsx
@@ -1,24 +1,25 @@
 import useSWR from 'swr';
 
-import React from 'react';
+import React, { useRef } from 'react';
 import { Message } from '../lib/store';
 
 const fetcher = (input: RequestInfo, init: RequestInit | undefined) => fetch(input, init).then((res) => res.json());
 
 // Hacky mutable state. Should only change client-side, but should refactor.
 // Would be bad if ever used server-side.
-let message: Message | null = null;
+let latestMessage: Message | null = null;
 
 export function Receiver(props: { receiverId: string }) {  
     const { data, error } = useSWR<Message,Error>(`/api/message?receiverId=${props.receiverId}`, fetcher, { refreshInterval: 1000 });
-
+    
+    let hasNewMessage = false; 
     if (data?.content) {
-        message = data;
+        latestMessage = data;
+        hasNewMessage = true;
     }
 
-    if (message) {
-        return <p>{message.sender}: {message.content}</p>
-    } else {
-        return null;
-    }
+    return latestMessage && <h3 style={{
+        transition: (latestMessage && !hasNewMessage) ? "all 0.5s": "",
+        opacity: (latestMessage && !hasNewMessage) ? 1.0 : 0.0
+    }}>{latestMessage.sender}: {latestMessage.content}</h3>
 }
