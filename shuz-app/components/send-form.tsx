761ca2629e6a948f8ebc99c5baa144c556d95ff3
lukesdm <2824165+lukesdm@users.noreply.github.com>
2022-03-14 12:22:29 +0100

    Reworked send-form state, using reducer

--- shuz-app/components/send-form.tsx
+++ shuz-app/components/send-form.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { OnResultFunction, QrReader } from 'react-qr-reader';
 import { EncryptError, SenderSecurityContext } from '../lib/security';
 import { Message } from '../lib/store';
@@ -52,75 +52,87 @@ async function sendMessage(receiverId: string, content: string): Promise<SendRes
     } else {
       error = new SendError('Unexpected problem. Please report this to our team, and we will investigate it as soon as possible.');
     }
-
-    // console.error(err);
   }
 
   return error ?? 'OK';
 }
 
-// Status and result of previous operation.
-// COULDDO: refactor to swap nulls with other results where it makes sense, e.g. 'HasQR'.
-type Status = ['WaitingForText', null ] | [ 'WaitingForQR', null ] | [ 'HasQR', null ]  | [ 'MessageSent', SendResult ];
+type ActionType = 'WaitForText' | 'WaitForQR' | 'HandleQR' | 'HandleSend';
+
+type Action = { type: 'WaitForText'; payload: string } // textbox contents updated
+  | { type: 'WaitForQR'; payload: null } // content already updated, nothing to pass.
+  | { type: 'HandleQR'; payload: string } // QR code.
+  | { type: 'HandleSend'; payload: SendResult };
+
+const initState = {
+  lastAction: 'WaitForText' as ActionType,
+  content: null as (string | null),
+  receiverId: null as (string | null),
+  sendResult: null as (SendResult | null)
+}
+
+type State = typeof initState;
+
+function reducer(state: State, action: Action): State {
+  const newState = { ...state, lastAction: action.type };
+  switch (action.type) {
+    case 'WaitForText':
+      return {...newState, content: action.payload };
+    case 'WaitForQR':
+      return {...newState };
+    case 'HandleQR':
+      return {...newState, receiverId: action.payload };
+    case 'HandleSend':
+      return {...newState, sendResult: action.payload };
+    default:
+      throw new Error('Unexpected action type.');
+  }
+}
 
 export function SendForm() {
-  const [content, setContent] = useState('');
-  const [receiverId, setReceiverId] = useState('');
-  const [[status, result], setStatus] = useState(['WaitingForText', null ] as Status);
-  const [notification, setNotification] = useState<string | null>(null);
+  
+  const [state, dispatch] = useReducer(reducer, initState);
 
   useEffect(() => {
-    switch (status) {
-      case 'WaitingForText': break;
-      case 'WaitingForQR':
-        setNotification(null);
-        break;
-      case 'HasQR':
-        (async () => {
-          const sendResult = await sendMessage(receiverId, content);
-          setStatus(["MessageSent", sendResult]);
-        })(); 
-        break;
-      case 'MessageSent':
-        const notific = result === 'OK' ? 'Message sent!' : `${result!.message}`;
-        setNotification(notific);
-        setStatus(["WaitingForText", null]);
-        break;
+    if (state.lastAction === 'HandleQR') {
+      (async () => {
+        if (!(state.receiverId && state.content)) {
+          throw new Error('Invalid state');
+        }
+
+        const sendResult = await sendMessage(state.receiverId, state.content);
+        dispatch({ type: 'HandleSend', payload: sendResult });
+      })();
     }
-  }, [status, result, notification, receiverId, content]);
+  }, [state]);
 
   const onQrRead: OnResultFunction = (result, error) => {
     if (!!result) {
-      setReceiverId(result.getText());
-      setStatus(["HasQR", null]);
+      dispatch({ type: 'HandleQR', payload: result.getText() });
     }
     if (!!error) {
       // ignore these errors for now.
     }
   }
 
-  const onSendClick = () => {
-    setStatus(["WaitingForQR", null]);
-  }
-
   return (
     <form onSubmit={e => e.preventDefault()}>
-      { status === "WaitingForQR" && <>
+      { state.lastAction === 'WaitForQR' && <>
         {/* <h3>Scan recipient&apos;s QR code</h3> */}
-        <QrReader videoStyle={{ height: '50vh', position: 'static' }} videoContainerStyle = {{ paddingTop: '', position: '' }} onResult= {onQrRead} constraints = {{ facingMode: { ideal: 'environment' } }} />
+        <QrReader videoStyle={{ height: '50vh', position: 'static' }} videoContainerStyle = {{ paddingTop: '', position: '' }} onResult={onQrRead} constraints = {{ facingMode: { ideal: 'environment' } }} />
       </> }
-      { status === "WaitingForText" && <>
+      { state.lastAction === 'WaitForText' && <>
         <label>
           Message:
-          <textarea name="content" rows={3} placeholder={"Your message...\n\n(From?)"} onChange={e => setContent(e.target.value)} />
+          <textarea name="content" rows={3} placeholder={"Your message...\n\n(From?)"} onChange={e => dispatch({ type: 'WaitForText', payload: e.target.value })} />
         </label>
-        <input type="button" value="Send" name="start-send" onClick={onSendClick} />
+        <input type="button" value="Send" name="start-send" onClick={() => dispatch({ type: 'WaitForQR', payload: null })} />
       </> }
       
       <p className='notification' style={{
-        transition: notification ? "all 1.0s": "",
-        opacity: notification ? 1.0 : 0.0 
-      }}>{notification}</p>
+        transition: state.sendResult ? "all 1.0s": "",
+        opacity: state.sendResult ? 1.0 : 0.0 
+      }}>{`${state.sendResult}`}</p>
       
     </form>
   );
