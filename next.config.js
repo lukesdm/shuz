c1ec1674913bb6b21942a98e607a9d7cff4c9e25
lukesdm <2824165+lukesdm@users.noreply.github.com>
2022-03-19 19:11:44 +0100

    Fix dev server,  turn off nonce stuff, it wasn't useful (applicable to style tags only).

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 // https://nextjs.org/docs/advanced-features/security-headers
 const securityHeaders = [
-  // CSP is set in _document.tsx, alongside nonce generation
-  // {
-  //   key: 'Content-Security-Policy',
-  //   value: `default-src 'self'`,
-  // },
+  {
+    key: 'Content-Security-Policy',
+    value: `default-src 'self'`,
+  },
   {
     key: 'Permissions-Policy',
     value: 'microphone=(), geolocation=(), interest-cohort=()',
@@ -29,12 +29,17 @@ const securityHeaders = [
 ];
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() { return [{ source: '/:path*',  headers: securityHeaders }] },
+const baseConfig = {
   reactStrictMode: true,
   env: {
     // REDIS_URL: 'redis://localhost:6379',
   }
 };
 
-module.exports = nextConfig;
+module.exports = (phase, { defaultConfig }) => {
+  return (phase === PHASE_DEVELOPMENT_SERVER) ? baseConfig
+  : { 
+      ...baseConfig,
+      headers: async () => [{ source: '/:path*',  headers: securityHeaders }],
+    }
+};
