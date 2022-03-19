90bdc1eb11b12d60bb0f3301dceb54e9285d7519
lukesdm <2824165+lukesdm@users.noreply.github.com>
2022-03-19 16:15:15 +0100

    Add security headers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,39 @@
+
+// https://nextjs.org/docs/advanced-features/security-headers
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: `default-src 'self'`, // Take care with syntax if updating this.
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'microphone=(), geolocation=(), interest-cohort=()',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'same-origin',
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() { return { source: '/:path*',  headers: securityHeaders }},
   reactStrictMode: true,
   env: {
     // REDIS_URL: 'redis://localhost:6379',
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
