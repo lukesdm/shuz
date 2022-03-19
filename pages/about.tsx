e4f49c73246e5d36f95ae4a16c995d2069a206dd
lukesdm <2824165+lukesdm@users.noreply.github.com>
2022-03-19 15:31:45 +0100

    Add footer to About

--- pages/about.tsx
+++ pages/about.tsx
@@ -23,8 +23,19 @@ const About: NextPage = () => {
         <p><em>shuz</em>{` is an attempt make the above easier to solve, when you are otherwise unconnected.`}</p>
         <p>{`It can also be useful when working with devices you don't regularly use, e.g. at a library - a quick way to transfer links and things without logging into another service.`}</p>
         <h3 id='#data-policy'>Data Policy</h3>
-        <p>Messages are end-to-end encrypted (in the browser). The encrypted contents are transferred via a temporary data store, where they are held for up to 30 seconds. <em>But</em>, this is an early stage app, and there are likely to be issues to resolve. Contributions in this area are especially welcome.</p>
+        <p>Messages are end-to-end encrypted (in the browser). The encrypted contents are transferred via a temporary data store, where they are held for up to 30 seconds. <em>But</em>, this is an early-stage app, and there are likely to be issues to resolve. Contributions in this area are especially welcome.</p>
       </main>
+      {/* SHOULDDO: Refactor this to a layout shared with Home */}
+      <footer className='container grid'>
+          <p>© <a href="https://github.com/lukesdm">Luke McQuade</a></p>
+          <p><Link href="/about"><a>About</a></Link></p>
+          <p><a href="/licenses.txt">Third-party licenses</a></p>
+          <p><a href="https://github.com/lukesdm/shuz">
+              {/* eslint-disable-next-line @next/next/no-img-element */ /* Awkward to get the style right with Next Image here, and it's a small file anyway. */}
+              <img src='/github-mark/GitHub-Mark-32px.png' alt='GitHub icon' width={24} height={24} />
+              &nbsp;<span style={{verticalAlign: 'middle'}}>{`Open Source`}</span>
+          </a></p>
+      </footer>
     </div>
   );
 }
